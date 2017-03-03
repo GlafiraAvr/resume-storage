3288292687ec001adc48585034896433791c3d52
Glafira <glafira.vikt.mal@gmail.com>
2017-03-03 23:19:31 +0200

    Realisation of method clear save delete getAll

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,48 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size=0;
+
 
     void clear() {
+        for (int i = 0; i <size ; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+         storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i <size ; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)){
+                System.arraycopy(storage, i + 1, storage, i + 1 - 1, size - (i + 1));
+                storage[size-1] = null;
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[size];
+        System.arraycopy(storage, 0, result, 0, size);
+        return result;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
